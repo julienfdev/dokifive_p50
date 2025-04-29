c86ee1b1eccfce12be8f710cf2d8cf8cac89f5c8
Julien <julien@dokitek.com>
2025-04-29 16:51:39 +0200

    ALU

---
+++ src/modules/comb/alu.sv
@@ -0,0 +1,29 @@
+import types::*;
+
+module alu(
+    input logic     [31:0]  a, b,
+    input alu_op_t          alu_op,
+    output logic    [31:0]  result,
+    output logic            zero
+);
+
+always_comb begin
+    case (alu_op)
+        ALU_OP_ADD: result = a + b;
+        ALU_OP_SUB: result = a - b;
+        ALU_OP_AND: result = a & b;
+        ALU_OP_OR : result = a | b;
+        ALU_OP_SLT: result = ($signed(a) < $signed(b)) ? 1 : 0;
+        ALU_OP_SLTU: result = (a < b) ? 1 : 0;
+        ALU_OP_XOR: result = a ^ b;
+        ALU_OP_SLL: result = a << b[4:0];
+        ALU_OP_SRL: result = a >> b[4:0];
+        ALU_OP_SRA: result = $signed(a) >>> b[4:0];
+        default   : result = 'hDEADBEEF; // Undefined operation
+    endcase
+
+    zero = (result == 32'b0) ? 1'b1 : 1'b0; // Set zero flag if result is zero
+end
+
+
+endmodule
--- src/modules/packages/types.sv
+++ src/modules/packages/types.sv
@@ -6,4 +6,16 @@ package types;
         J_TYPE,
         U_TYPE
     } immsrc_t;
+    typedef enum logic [3:0] {
+        ALU_OP_ADD,
+        ALU_OP_SUB,
+        ALU_OP_AND,
+        ALU_OP_OR,
+        ALU_OP_SLT,
+        ALU_OP_SLTU,
+        ALU_OP_XOR,
+        ALU_OP_SLL,
+        ALU_OP_SRL,
+        ALU_OP_SRA
+    } alu_op_t;
 endpackage

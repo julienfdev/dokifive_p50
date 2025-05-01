e107e5c3caf7e1d94e4cb11b6a6da2c65216d8ca
Julien <julien@dokitek.com>
2025-05-01 16:59:30 +0200

    WIP

---
+++ riscv/main.s
@@ -0,0 +1,35 @@
+.globl _start
+
+.equ _start, 0x00000000
+
+// RAM address space
+.equ RAM_START, 0x200
+
+_start:
+addi s0, zero, 1337 # t0 = zero + 1337
+addi t0, zero, RAM_START # prepare the RAM address to store 1337
+nop
+nop # we don't do hazard handling yet and sw depends on both addis
+sw s0, 4(t0)
+addi s1, zero, 1000
+nop # I think we don't need that
+lw s1, 4(t0)
+addi s1, s1, 337
+nop
+nop # we will use the result from the addi
+beq s0, s1, branch; # if s0 == s1 then branch
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+branch:
+addi s0, s0, 100
+addi s1, s1, 200
+
+
+
+
